c70dee18355070c8db190c08e9cb606b7984d5b6
manas <manasharsit.raj@indusnet.co.in>
2024-09-23 18:08:52 +0530

    changed to axios and header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,13 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 import Home from "./pages/Home";
+
 
 const App = () => {
   return (
     <div className="homepage">
-      <Header />
-      <main>
         <Home />
-      </main>
-      <Footer />
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,23 @@
+// eslint-disable-next-line no-unused-vars
 import React, { useState, useEffect } from "react";
 import Product from "./Product";
-
+import axios from "axios";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 const Home = () => {
   const [products, setProducts] = useState([]);
   useEffect(() => {
     console.log(" Im here");
     // Fetch products from the API
-    fetch("http://localhost:3000/api/products")
-      .then((response) => response.json())
-      .then((data) => {
-        setProducts(data);
+    // fetch("http://localhost:3000/api/products")
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     setProducts(data);
+    //   })
+      
+    axios.get("http://localhost:3000/api/products")
+      .then((response) => {
+        setProducts(response.data); // Access data from the response
       })
       .catch((error) => {
         console.error("Error fetching products:", error);
@@ -17,11 +25,14 @@ const Home = () => {
   }, []);
 
   return (
-    <>
+    <div className="homepage"> {/* Ensure the homepage class wraps the content */}
+    <Header />
+    <main>
       <h2>Products</h2>
       <Product products={products} />
-    </>
+    </main>
+    <Footer />
+  </div>
   );
 };
-
 export default Home;
